2e88b83bb6edb2339f18e3ccf3681a9658c3f2f7
cppdozer <kaanmertagyol@gmail.com>
2023-08-27 07:34:46 +0300

    Project done.

---
+++ createDB.php
@@ -0,0 +1,38 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "Kumsal11!";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$dbname = "demoEmp";
+
+$checkDbQuery = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$dbname'";
+$checkDbResult = mysqli_query($conn, $checkDbQuery);
+
+if (!$checkDbResult) {
+    die("Error: " . mysqli_error($conn));
+}
+
+$dbExists = mysqli_num_rows($checkDbResult) > 0;
+
+if (!$dbExists) {
+    $createDbQuery = "CREATE DATABASE $dbname";
+
+    if (mysqli_query($conn, $createDbQuery)) {
+        echo "Database created successfully";
+    } else {
+        echo "Error creating database: " . mysqli_error($conn);
+    }
+} else {
+    echo "Database already exists";
+}
+
+mysqli_close($conn);
+?>
---
+++ createTable.php
@@ -0,0 +1,46 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "Kumsal11!";
+$dbname = "demoEmp";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$tableName = "employees";
+
+$checkTableQuery = "SHOW TABLES LIKE '$tableName'";
+$checkTableResult = mysqli_query($conn, $checkTableQuery);
+
+if (!$checkTableResult) {
+    die("Error: " . mysqli_error($conn));
+}
+
+$tableExists = mysqli_num_rows($checkTableResult) > 0;
+
+if (!$tableExists) {
+    $createTableQuery = "
+        CREATE TABLE $tableName (
+            id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+            emp_name VARCHAR(100) NOT NULL,
+            emp_address VARCHAR(255) NOT NULL,
+            salary INT(10) NOT NULL
+        )
+    ";
+
+    if (mysqli_query($conn, $createTableQuery)) {
+        echo "Table created successfully";
+    } else {
+        echo "Error creating table: " . mysqli_error($conn);
+    }
+} else {
+    echo "Table already exists";
+}
+
+mysqli_close($conn);
+?>
