8c4aa9dcdc34500b0a38ee00bba219633da0cb0a
cppdozer <kaanmertagyol@gmail.com>
2023-08-27 07:35:57 +0300

    removed unnecessary files.

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'Kumsal11!');
+define('DB_PASSWORD', 'PASSWORD');
 define('DB_NAME', 'demoEmp');
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
